a387fdaf154c0c7295532c173d7e4f3c249e19fa
ANDRÉS BERCICH <andybercich6@gmail.com>
2024-10-24 19:40:40 -0300

    Organizando Desarrollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
-import { useState } from "react";
-import style from "./App.module.css";
-import { CrearEmpresa } from "./components/ui/PopUps/CrearEmpresa/CrearEmpresa";
-import { CrearSucursal } from "./components/ui/PopUps/CrearSucursal/CrearSucursal";
+import { StrictMode } from "react";
+import { Home } from "./Components/Screens/Home";
+
 
 export const App = ()=> {
-  const [modalEmpresa, setModalEmpresa] = useState(false); 
-  const [modalSucursal, setmodalSucursal] = useState(false); 
 
   return (
-    <div className={style.containerApp}>
-    <div className={style.sectionEmpresa}>
-      <div className={style.headerEmpresa}>
-        <h2>Empresas</h2>
-        <button onClick={()=>{
-          setModalEmpresa(true)
-        }}>
-            Agregar Empresa
-          </button>
-      </div>
+    <StrictMode>
+      <Home>
+        
+      </Home>
+    </StrictMode>
 
-      <div className={style.sectionCardsEmpresa}></div>
-    </div>
-
-    {modalEmpresa && <CrearEmpresa onClose={() => setModalEmpresa(false)} />}
-    {modalSucursal && <CrearSucursal onClose={() => setmodalSucursal(false)} />}
-
-    <div className={style.sectionSucursal}>
-      <div className={style.headerSucursal}>
-        <h2>Sucursales</h2>
-        <button onClick={()=>{
-          setmodalSucursal(true)
-        }}
-        >Agregar Sucursal</button>
-      </div>
-
-      <div className={style.sectionCardsSucursal}></div>
-    </div>
-  </div>
   );
 }
---
+++ src/Components/Screens/Home.tsx
@@ -0,0 +1,43 @@
+import style from "./Home.module.css";
+import { CrearEmpresa } from "../../Components/UI/PopUps/CrearEmpresa/CrearEmpresa";
+import { CrearSucursal } from "../../Components/UI/PopUps/CrearSucursal/CrearSucursal";
+import { useState } from "react";
+
+export const Home = () => {
+
+    const [modalEmpresa, setModalEmpresa] = useState(false); 
+    const [modalSucursal, setmodalSucursal] = useState(false); 
+  
+
+  return (
+    <div className={style.containerApp}>
+    <div className={style.sectionEmpresa}>
+      <div className={style.headerEmpresa}>
+        <h2>Empresas</h2>
+        <button onClick={()=>{
+          setModalEmpresa(true)
+        }}>
+            Agregar Empresa
+          </button>
+      </div>
+
+      <div className={style.sectionCardsEmpresa}></div>
+    </div>
+
+    {modalEmpresa && <CrearEmpresa onClose={() => setModalEmpresa(false)} />}
+    {modalSucursal && <CrearSucursal onClose={() => setmodalSucursal(false)} />}
+
+    <div className={style.sectionSucursal}>
+      <div className={style.headerSucursal}>
+        <h2>Sucursales</h2>
+        <button onClick={()=>{
+          setmodalSucursal(true)
+        }}
+        >Agregar Sucursal</button>
+      </div>
+
+      <div className={style.sectionCardsSucursal}></div>
+    </div>
+  </div>
+  )
+}
