c1d4a81e315ddd9902cc6f96344c07a8bfa31480
Ulises Guzman <guzmangimenezulises06@gmail.com>
2024-10-29 08:46:46 -0300

    hola

--- src/Redux/Slice/sucursalsEmpresaElegida.ts
+++ src/Redux/Slice/sucursalsEmpresaElegida.ts
@@ -2,27 +2,18 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit"
 import { IEmpresa } from "../../Models/types/dtos/empresa/IEmpresa";
 
 interface UserDataInitial{
-    
     empresa: IEmpresa | null
-    
-
 }
-
 const initialState: UserDataInitial = {empresa: null }
-
 const Empresa = createSlice({
     name: "empresa",
     initialState,
     reducers: {
-
         setSelectedEmpresa: (state, action: PayloadAction<IEmpresa>)=>{
             console.log(action.payload)
             state.empresa = action.payload;
         }
-
-
     }
-
 })
 
 export const {setSelectedEmpresa}= Empresa.actions;
