a9eb646c6d1057be14d725522e637fe720e36aa0
ANDRÉS BERCICH <andybercich6@gmail.com>
2024-11-13 16:40:35 -0300

    Estilos categorias

--- src/Components/UI/Categorias/AcorditionCategories.tsx
+++ src/Components/UI/Categorias/AcorditionCategories.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react"
 import styles from "./Acordition.module.css"
 import { ICategorias } from "../../../Models/types/dtos/categorias/ICategorias"
+import { EditIcon } from "../Icons/EditIcon/EditIcon"
+import { AddIcon } from "../Icons/AddIcon/AddIcon"
 
 interface Props{
     categoria: ICategorias
@@ -10,17 +12,27 @@ export const AcorditionCategories: FC<Props> = ({categoria})=> {
   return (
     <div  className={styles.categoriaPadre}>
 
-        <div>
+        <div className={styles.titleContainer}>
             <h3>{categoria.denominacion}</h3>
         </div>
 
-        <div>
-            
+        <div className={styles.actionsContainer}>
+          
+            <AddIcon></AddIcon>
+            <EditIcon></EditIcon>
+
         </div>
         
-        <div>
-            
+
+        <div className={styles.arrowContainer}>
+          {/*<span
+            className={`material-symbols-outlined ${styles.arrowButton} ${!show ? styles.hidden : ''}`}
+            onClick={()=>{setShow(!show)}}
+          >
+            {show ? 'arrow_back_ios' : 'arrow_forward_ios'}
+          </span> */}
         </div>
+
 
 
     </div>
